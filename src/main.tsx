6a3a9dd46d98bb2c882d8f477db0de105cbc174e
Emery Frank <frankflej@gmail.com>
2023-07-28 09:56:15 +0200

    getting all the data from the db postgress

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/', // Replace with your Neon server URL
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ApolloProvider  client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider >,
 )
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,18 +2,27 @@
 import Card from '../components/card'
 import mycards from '../utils/dummyData'
 import Heading from '../container/Heading'
-
+import { useQuery, gql } from '@apollo/client';
+const GET_ALL_FLASHCARDS = gql`
+query {
+  feed {
+    id
+    title
+    details
+  }
+}
+`;
 export default function HomePage() {
-  
+  const { loading, error, data } = useQuery(GET_ALL_FLASHCARDS);
   return (
     <div className='wrapper'>
         <Heading show={true}/>
     <div className='allCards'>
-      {
-        mycards.map((card)=>{
-          return <Card title={card.title} definition={card.definition}/>
-        })
-      }
+    {loading && <p>Loading...</p>}
+    {data &&    data.feed.map((card:any)=>{
+      return <Card title={card.title} definition={card.details}/>
+    })}
+    {error && <p>Error: {error.message}</p>}
     </div>
     </div>
   )
